4300fa24d8eeb2a6684aa2742b98e8bceca49a17
Kurt Ball <kurtisdball@hotmail.com>
2021-10-02 13:32:09 -0600

    bring data to chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,6 +1,10 @@
-import { Chart } from "chart.js"
+import { useContext } from "react"
+// import { Chart } from "chart.js"
+import { CalculatorContext } from "../context/CalculatorContext"
 
 const LineChart = () => {
+	const { dataList } = useContext(CalculatorContext)
+	console.log("The data:", dataList)
 	return (
 		<div>
 			<h2>chart</h2>
--- src/context/CalculatorContext.js
+++ src/context/CalculatorContext.js
@@ -1,99 +1,109 @@
-import { useState, createContext, useEffect } from 'react'
-import axios from 'axios'
+import { useState, createContext, useEffect } from "react"
+import axios from "axios"
 
 export const CalculatorContext = createContext()
 
 const CalculatorContextProvider = (props) => {
-  // Calculate Future value of an investment
-  const [amount, setAmount] = useState(0)
-  const [compoundRate, setCompoundRate] = useState(1)
-  const [isOpen, setIsOpen] = useState(false)
-  const [interestRate, setInterestRate] = useState('')
-  const [principle, setPrinciple] = useState('')
-  const [selected, setSelected] = useState(null)
-  const [time, setTime] = useState('')
+	const date = new Date()
+	const month = date.getMonth() + 1
+	const year = date.getFullYear()
+	// Calculate Future value of an investment
+	const [amount, setAmount] = useState(0)
+	const [compoundRate, setCompoundRate] = useState(1)
+	const [isOpen, setIsOpen] = useState(false)
+	const [interestRate, setInterestRate] = useState("")
+	const [principle, setPrinciple] = useState("")
+	const [selected, setSelected] = useState(null)
+	const [time, setTime] = useState("")
 
-  //Used to calculate inflation
-  const [dollarValue, setDollarValue] = useState('')
-  const [inflationValue, setInflationValue] = useState(0)
-  const [startCpi, setStartCpi] = useState(1)
-  const [endCpi, setEndCpi] = useState(0)
-  const [search, setSearch] = useState(
-    `2000-01-01&endReferencePeriod=2021-01-01`,
-  )
-  const [inflationFocus, setInflationFocus] = useState('41690973')
+	//Used to calculate inflation
+	const [data, setData] = useState()
+	const [dollarValue, setDollarValue] = useState("")
+	const [inflationValue, setInflationValue] = useState(0)
+	const [startCpi, setStartCpi] = useState(1)
+	const [endCpi, setEndCpi] = useState(0)
+	const [search, setSearch] = useState(
+		`2000-01-01&endReferencePeriod=${year}-${month}-01`
+	)
 
-  const calcCompoundInterest = () => {
-    const calcAmount =
-      principle * (1 + interestRate / compoundRate) ** (compoundRate * time)
-    setAmount(calcAmount.toFixed(2))
-  }
-  //calculate compound interest rate with periodic deposits
-  const calcCompoundInterestPeriodicDeposits = () => {
-    for (let i = time; i >= 0; i--) {
-      const calcAmount =
-        principle * (1 + interestRate / compoundRate) ** (compoundRate * i)
-      setAmount(calcAmount.toFixed(2))
-    }
-  }
+	const [inflationFocus, setInflationFocus] = useState("41690973")
 
-  const combined = [dollarValue || search]
-  useEffect(() => {
-    // retrieve information from statscan
-    const getData = async () => {
-      try {
-        const res = await axios.get(
-          `https://www150.statcan.gc.ca/t1/wds/rest/getDataFromVectorByReferencePeriodRange?vectorIds="${inflationFocus}"&startRefPeriod=${search}`,
-        )
+	let dataPoint = { date: null, value: null }
+	let dataList = []
+	const calcCompoundInterest = () => {
+		const calcAmount =
+			principle * (1 + interestRate / compoundRate) ** (compoundRate * time)
+		setAmount(calcAmount.toFixed(2))
+	}
 
-        const cpiData = res.data[0].object.vectorDataPoint
-        setStartCpi(cpiData[0].value)
-        setEndCpi(cpiData[cpiData.length - 1].value)
-      } catch (err) {
-        console.log('This is the error message:', err)
-      }
-    }
-    getData()
-    // console.log(`This is the start value ${startCpi}`)
-    // console.log(`This is the end value ${endCpi}`)
-    const calcInflation = () => {
-      const inflationRatio = (endCpi - startCpi) / startCpi
-      const inflation = dollarValue * inflationRatio.toFixed(2) + dollarValue
-      return setInflationValue(inflation)
-    }
+	//calculate compound interest rate with periodic deposits
+	const calcCompoundInterestPeriodicDeposits = () => {
+		for (let i = time; i >= 0; i--) {
+			const calcAmount =
+				principle * (1 + interestRate / compoundRate) ** (compoundRate * i)
+			setAmount(calcAmount.toFixed(2))
+		}
+	}
 
-    calcInflation()
-  }, [combined])
+	const combined = [dollarValue || search]
+	useEffect(() => {
+		// retrieve information from statscan
+		const getData = async () => {
+			try {
+				const res = await axios.get(
+					`https://www150.statcan.gc.ca/t1/wds/rest/getDataFromVectorByReferencePeriodRange?vectorIds="${inflationFocus}"&startRefPeriod=${search}`
+				)
 
-  return (
-    //Variables and hooks to add to components
-    <CalculatorContext.Provider
-      value={{
-        setInflationFocus,
-        setDollarValue,
-        search,
-        setSearch,
-        amount,
-        setAmount,
-        calcCompoundInterest,
-        principle,
-        setPrinciple,
-        interestRate,
-        isOpen,
-        setIsOpen,
-        setInterestRate,
-        compoundRate,
-        setCompoundRate,
-        time,
-        setTime,
-        inflationValue,
-        selected,
-        setSelected,
-      }}
-    >
-      {props.children}
-    </CalculatorContext.Provider>
-  )
+				const cpiData = await res.data[0].object.vectorDataPoint
+				dataList.push(cpiData)
+
+				setStartCpi(cpiData[0].value)
+				setEndCpi(cpiData[cpiData.length - 1].value)
+			} catch (err) {
+				console.error("This is the error message:", err)
+			}
+		}
+		getData()
+		// console.log(`This is the start value ${startCpi}`)
+		// console.log(`This is the end value ${endCpi}`)
+		const calcInflation = () => {
+			const inflationRatio = (endCpi - startCpi) / startCpi
+			const inflation = dollarValue * inflationRatio.toFixed(2) + dollarValue
+			return setInflationValue(inflation)
+		}
+
+		calcInflation()
+	}, [combined])
+
+	return (
+		//Variables and hooks to add to components
+		<CalculatorContext.Provider
+			value={{
+				amount,
+				setAmount,
+				compoundRate,
+				dataList,
+				setCompoundRate,
+				calcCompoundInterest,
+				setInflationFocus,
+				setDollarValue,
+				search,
+				setSearch,
+				principle,
+				setPrinciple,
+				interestRate,
+				setInterestRate,
+				isOpen,
+				setIsOpen,
+				time,
+				setTime,
+				inflationValue,
+				selected,
+				setSelected,
+			}}>
+			{props.children}
+		</CalculatorContext.Provider>
+	)
 }
 
 export default CalculatorContextProvider
